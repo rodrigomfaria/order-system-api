778d5c84fa77b6e5801671a8acf6d9ba7e6fe20d
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-11-25 14:16:12 -0300

    Creating instantiation operation

--- src/main/java/br/com/rmf/ordersystemapi/OrderSystemApiApplication.java
+++ src/main/java/br/com/rmf/ordersystemapi/OrderSystemApiApplication.java
@@ -1,13 +1,33 @@
 package br.com.rmf.ordersystemapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rmf.ordersystemapi.entities.Category;
+import br.com.rmf.ordersystemapi.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class OrderSystemApiApplication {
+public class OrderSystemApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderSystemApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
