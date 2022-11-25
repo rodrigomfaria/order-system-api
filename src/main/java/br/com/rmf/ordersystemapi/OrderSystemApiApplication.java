345572a3ff1ab32a909ab8c6c229781d27f7a9e2
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-11-25 11:37:13 -0300

    project created

---
+++ src/main/java/br/com/rmf/ordersystemapi/OrderSystemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rmf.ordersystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rmf/ordersystemapi/OrderSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rmf.ordersystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
