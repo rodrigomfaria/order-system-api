2a3c02415057d22603a417cc71bee26437c2f06e
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-11-25 12:26:45 -0300

    Testing the first domain class - Category

--- src/main/java/br/com/rmf/ordersystemapi/controllers/CategoryController.java
+++ src/main/java/br/com/rmf/ordersystemapi/controllers/CategoryController.java
@@ -1,16 +1,29 @@
 package br.com.rmf.ordersystemapi.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rmf.ordersystemapi.entities.Category;
 
 @RestController
 @RequestMapping(value = "categories")
 public class CategoryController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String toList() {
-		return "Rest is working!!!";
+	public List<Category> toList() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
