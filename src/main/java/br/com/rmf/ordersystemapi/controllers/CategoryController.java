2bd023f7345fa522067b90058b1b2b11e24cd9bf
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-11-25 13:19:24 -0300

    creating repository and service for category

--- src/main/java/br/com/rmf/ordersystemapi/controllers/CategoryController.java
+++ src/main/java/br/com/rmf/ordersystemapi/controllers/CategoryController.java
@@ -1,29 +1,28 @@
 package br.com.rmf.ordersystemapi.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rmf.ordersystemapi.entities.Category;
+import br.com.rmf.ordersystemapi.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "categories")
 public class CategoryController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Category> toList() {
-		
-		Category cat1 = new Category(1, "Computing");
-		Category cat2 = new Category(2, "Office");
-		
-		List<Category> list = new ArrayList<>();
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	@Autowired
+	private CategoryService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+
+		Category obj = service.find(id);
+
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/br/com/rmf/ordersystemapi/services/CategoryService.java
@@ -0,0 +1,22 @@
+package br.com.rmf.ordersystemapi.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rmf.ordersystemapi.entities.Category;
+import br.com.rmf.ordersystemapi.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repo;
+
+	public Category find(Integer id) {
+		Optional<Category> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
