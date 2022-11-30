fdc50299d1c3e67c3451ce52c48378c510c40729
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-11-30 11:29:30 -0300

    inserting a new category with post

--- src/main/java/br/com/rmf/ordersystemapi/controllers/CategoryController.java
+++ src/main/java/br/com/rmf/ordersystemapi/controllers/CategoryController.java
@@ -1,11 +1,15 @@
 package br.com.rmf.ordersystemapi.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.rmf.ordersystemapi.entities.Category;
 import br.com.rmf.ordersystemapi.services.CategoryService;
@@ -23,6 +27,14 @@ public class CategoryController {
 		Category obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
