e0bf99a1af85ee944733187615b95d2b2ee6944e
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-11-30 12:00:57 -0300

    deleting a category with delete method

--- src/main/java/br/com/rmf/ordersystemapi/services/CategoryService.java
+++ src/main/java/br/com/rmf/ordersystemapi/services/CategoryService.java
@@ -3,10 +3,12 @@ package br.com.rmf.ordersystemapi.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.rmf.ordersystemapi.entities.Category;
 import br.com.rmf.ordersystemapi.repositories.CategoryRepository;
+import br.com.rmf.ordersystemapi.services.exceptions.DataIntegrityException;
 import br.com.rmf.ordersystemapi.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoryService {
 	public Category update(Category obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
