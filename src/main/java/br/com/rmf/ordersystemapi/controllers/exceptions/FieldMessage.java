7df81170c88e13ac71524063f0f4ccfab6a3eeab
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-12-11 12:10:40 -0300

    Customizing the validation return

---
+++ src/main/java/br/com/rmf/ordersystemapi/controllers/exceptions/FieldMessage.java
@@ -0,0 +1,38 @@
+package br.com.rmf.ordersystemapi.controllers.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String message;
+
+	public FieldMessage() {
+
+	}
+
+	public FieldMessage(String FieldName, String message) {
+		super();
+		this.fieldName = FieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
